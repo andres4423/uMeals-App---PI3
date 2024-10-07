18f926527ac7a181ddac4f57ff4f32afbc9e3168
Juancamilo3821 <juan.gomezr.2021@upb.edu.co>
2024-10-06 20:16:50 -0500

    Jwt - Middleware

--- lib/services/business_api.dart
+++ lib/services/business_api.dart
@@ -3,17 +3,28 @@ import 'package:umeals/domain/types/business_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BusinessPage extends StatelessWidget {
   const BusinessPage({super.key});
 
   Future<List<BusinessModel>> fetchBusinesses() async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:3000/emprendimientos'));
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt');
+
+    final response = await http.get(
+      Uri.parse('http://10.0.2.2:3000/emprendimientos'),
+      headers: {
+        'Authorization': 'Bearer $token', 
+      },
+    );
+
     if (response.statusCode == 200) {
       List data = json.decode(response.body);
       print(data);
       return data.map((business) => BusinessModel.fromJson(business)).toList();
+    } else if (response.statusCode == 401) {
+      throw Exception('Token expired, please log in again');
     } else {
       throw Exception('Failed to load businesses');
     }
--- lib/services/carrusel_api.dart
+++ lib/services/carrusel_api.dart
@@ -2,19 +2,30 @@ import 'package:umeals/Screens/mainPage_parts/exports.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../domain/types/swiperInfo.dart';
-
 
 class CarruselApi extends StatelessWidget {
   const CarruselApi({super.key});
 
   Future<List<InfoSwiper>> fetchCarruselImages() async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:3000/getCarruselImages'));
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt');
+
+    final response = await http.get(
+      Uri.parse('http://10.0.2.2:3000/getCarruselImages'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       print(data);
       return data.map((images) => InfoSwiper.fromMap(images)).toList();
+    } else if (response.statusCode == 401) {
+      throw Exception('Token expired, please log in again');
     } else {
       throw Exception('Failed to load Images');
     }
@@ -29,10 +40,10 @@ class CarruselApi extends StatelessWidget {
           return const Center(child: CircularProgressIndicator());
         } else if (snapshot.hasError) {
           print(snapshot.error);
-          return const Center(child: Text('Error loading businesses'));
+          return const Center(child: Text('Error loading images'));
         } else {
           final imagesPath = snapshot.data!;
-          return SwiperInfo(infoSwiper: imagesPath );
+          return SwiperInfo(infoSwiper: imagesPath);
         }
       },
     );
